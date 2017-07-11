147081aa8aa9c084907fc9f8572ab07119179093
Joe Fifer <jfifer@coredial.com>
2017-07-11 15:56:09 -0400

    UI cleanup

--- resources/views/portal.blade.php
+++ resources/views/portal.blade.php
@@ -28,16 +28,23 @@
             <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        <h3>Query Builder</h3>
+                        <h3 class="pull-left">Query Builder</h3>
                         <button class="btn btn-default pull-right" ng-click="submitQuery(query)">Submit</button>
                         <div class="clearfix"></div>
                     </div>
                     <div class="panel-body">
                         <div class="row" ng-repeat="column in columns track by $index" ng-if="$index % 4 == 0">
-                            <div class="col-xs-3" 
+                            <div class="col-md-3 panel-default" 
                                  ng-repeat="i in [$index, $index + 1, $index + 2, $index + 3]" 
                                  ng-if="columns[i] != null">
-                                <div><label><%columns[i].column%></label> <input name="<%query[columns[i].column]%>" ng-model="query[columns[i].column]" /></div>
+                                 <div class="panel-heading">
+                                    <span class="pull-left"><%columns[i].column%></span>
+                                    <input type="checkbox" name="included" class="pull-right" />
+                                    <div class="clearfix"></div>
+                                 </div>
+                                <div class="panel-content">
+                                    <input name="<%query[columns[i].column]%>" ng-model="query[columns[i].column]" />
+                                </div>
                             </div>
                         </div>
                     </div>
